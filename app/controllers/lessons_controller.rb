4246f0c5fb9b37b39b921117e0a138b8a1b6d6da
chiyingleung1 <chiying.leung@gmail.com>
2016-10-08 17:53:05 +0000

    Added permissions to lessons from courses show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
@@ -6,6 +9,13 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_current_lesson
+    @course = current_lesson.section.course
+    if !current_user.enrolled_in?(@course)
+      redirect_to course_path(@course), alert: 'Only users enrolled in this course can view lessons. Please enroll below.'
+    end
   end
 
 end
